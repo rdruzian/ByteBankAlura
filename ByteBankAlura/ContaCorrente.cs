2d2d4ac8adc9b83507b636b688ea19445e7cae79
rdruzian <rendruzian@gmail.com>
2021-07-05 10:59:08 -0300

    Adicionado summary, que é utilizado como documentação do código criado

--- ByteBankAlura/ContaCorrente.cs
+++ ByteBankAlura/ContaCorrente.cs
@@ -4,6 +4,13 @@
     public int numero;
     public double saldo;
 
+
+    /// <summary>
+    /// Método Sacar para realizar o saque de sua conta, deve-se receber apenas o valor que será sacado.
+    /// Retorna um boolenao para indicar se foi realizado a transação corretamente ou não
+    /// </summary>
+    /// <param name="valor"></param>
+    /// <returns></returns>
     public bool Sacar (double valor)
     {
         if(valor >= this.saldo)
@@ -15,6 +22,15 @@
         {
             return false;
         }
+    }
+
+    /// <summary>
+    /// Método para realizar um Depósito na conta, deve-se passar apenas o valor que erá depositado
+    /// </summary>
+    /// <param name="valor"></param>
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
     }
 
 }
