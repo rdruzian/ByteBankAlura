bb817ab7da89d6997b74e042ff1d5c3818483b65
rdruzian <rendruzian@gmail.com>
2021-07-05 10:54:17 -0300

    Criado primeiro método

--- ByteBankAlura/ContaCorrente.cs
+++ ByteBankAlura/ContaCorrente.cs
@@ -3,4 +3,18 @@
     public int agencia;
     public int numero;
     public double saldo;
+
+    public bool Sacar (double valor)
+    {
+        if(valor >= this.saldo)
+        {
+            this.saldo -= valor;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
--- ByteBankAlura/Program.cs
+++ ByteBankAlura/Program.cs
@@ -13,6 +13,17 @@ namespace ByteBankAlura
             contaGabriela.numero = 863452;
             contaGabriela.saldo = 100;
 
+            if (contaGabriela.Sacar(100))
+            {
+                Console.WriteLine("Saque realizado com sucesso!");
+                Console.WriteLine(contaGabriela.saldo);
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível realizar o saque!");
+                Console.WriteLine(contaGabriela.saldo);
+            }
+
             Console.WriteLine("Titular: " + contaGabriela.titular);
             Console.WriteLine("Agencia: " + contaGabriela.agencia);
             Console.WriteLine("Numero: " + contaGabriela.numero);
