d5c868c951de257947234a797ccfab0b7dfb54bd
renat <renat@DESKTOP-TC5LIK4>
2021-07-05 10:12:04 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBankAlura/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ByteBankAlura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaGabriela = new ContaCorrente();
+
+            contaGabriela.titular = "Gabriela";
+            contaGabriela.agencia = 863;
+            contaGabriela.numero = 863452;
+            contaGabriela.saldo = 100;
+
+            Console.WriteLine("Titular: " + contaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaGabriela.agencia);
+            Console.WriteLine("Numero: " + contaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaGabriela.saldo);
+            Console.ReadLine();
+        }
+    }
+}
